9fe659609f9575ba3399848ee4b15a846f722a27
logfire <constient-altrova@constient.com>
2023-09-05 22:23:54 +0530

    Brew formula update for logfire version v4.3

--- Formula/logfire.rb
+++ Formula/logfire.rb
@@ -13,7 +13,7 @@ class Logfire < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/logfire-sh/cli/releases/download/v4.3/logfire_darwin_arm64.tar.gz", using: CurlDownloadStrategy
-      sha256 "2690a9db261bddf44ba5c1fa4c08e5972d88860a399e0e90220fe06a18736d5f"
+      sha256 "db8c04846d6296ae9653e2a90eda022b6201764a24df8b29a59d8938cd69e875"
 
       def install
         bin.install "logfire"
@@ -24,7 +24,7 @@ class Logfire < Formula
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
       url "https://github.com/logfire-sh/cli/releases/download/v4.3/logfire_linux_arm64.tar.gz", using: CurlDownloadStrategy
-      sha256 "e25ea75ea745b9f9d9af8105ad579ac2967cd3e918c9a5d089e26d7a9e0179b4"
+      sha256 "2aa157b1b65d28e594ebc2f7d29b55cc01270ed13ac606783e0807c0d67adf61"
 
       def install
         bin.install "logfire"
