a19dbbb1d2e6de30b67b2ae2241eabf89e918afd
logfire <constient-altrova@constient.com>
2023-09-13 12:58:48 +0530

    Brew formula update for logfire version v6.7

--- Formula/logfire.rb
+++ Formula/logfire.rb
@@ -5,15 +5,15 @@
 class Logfire < Formula
   desc "Logfire command line interface"
   homepage "https://github.com/logfire-sh/cli"
-  version "6.6"
+  version "6.7"
   license "Apache 2.0"
 
   depends_on "git"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/logfire-sh/cli/releases/download/v6.6/logfire_darwin_arm64.tar.gz", using: CurlDownloadStrategy
-      sha256 "ed7cb10f66554e432a979d8d27625c638c23cf026ac5a2d6a960d108e0b97b60"
+      url "https://github.com/logfire-sh/cli/releases/download/v6.7/logfire_darwin_arm64.tar.gz", using: CurlDownloadStrategy
+      sha256 "d2e1ce4b0d9e66838f75643e120e82d55f993d8a583e2bf061ec6d0d31d3de64"
 
       def install
         bin.install "logfire"
@@ -23,8 +23,8 @@ class Logfire < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/logfire-sh/cli/releases/download/v6.6/logfire_linux_arm64.tar.gz", using: CurlDownloadStrategy
-      sha256 "fbd39c463e9635f26007b1267a500aeeee44554b9bbb18d588479c7ec2806d41"
+      url "https://github.com/logfire-sh/cli/releases/download/v6.7/logfire_linux_arm64.tar.gz", using: CurlDownloadStrategy
+      sha256 "463bc6465ecdec9103324c80fded2fd60510fee1504669c293950b635af228f4"
 
       def install
         bin.install "logfire"
