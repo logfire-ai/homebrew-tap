a16fb9f9465f9281d810a73d37a5e57f27ebbf75
logfire <support@logfire.ai>
2024-06-07 08:40:25 +0530

    Brew formula update for logfire version v1.0.8

--- Formula/logfire.rb
+++ Formula/logfire.rb
@@ -5,15 +5,15 @@
 class Logfire < Formula
   desc "Logfire command line interface"
   homepage "https://github.com/logfire-ai/cli"
-  version "1.0.7"
+  version "1.0.8"
   license "Apache 2.0"
 
   depends_on "git"
 
   on_macos do
     on_arm do
-      url "https://github.com/logfire-ai/cli/releases/download/v1.0.7/logfire_darwin_arm64.tar.gz", using: CurlDownloadStrategy
-      sha256 "d88fdf766067f3f8c683155af3d4000666d2c4d7f181d72867b614fe40fe054a"
+      url "https://github.com/logfire-ai/cli/releases/download/v1.0.8/logfire_darwin_arm64.tar.gz", using: CurlDownloadStrategy
+      sha256 "15cde8850d32ec5d365cb3b59c88cdc95330164175fa96db0c8e0b8826ba50e3"
 
       def install
         bin.install "logfire"
@@ -24,8 +24,8 @@ class Logfire < Formula
   on_linux do
     on_arm do
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/logfire-ai/cli/releases/download/v1.0.7/logfire_linux_arm64.tar.gz", using: CurlDownloadStrategy
-        sha256 "f33578a370713e7359e8bb22c4731706dd948379d526107ca12865543f1e7f71"
+        url "https://github.com/logfire-ai/cli/releases/download/v1.0.8/logfire_linux_arm64.tar.gz", using: CurlDownloadStrategy
+        sha256 "0ee19d80d62f72c41a064ec4ae250c561e938acf80e1c2db2abbb7ea663b8b3a"
 
         def install
           bin.install "logfire"
